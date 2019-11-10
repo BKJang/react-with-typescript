51e23ad9bf0a14de70149f91b9bac8d4f75dc683
BKJang <jp302119@gmail.com>
2019-11-10 17:32:57 +0900

    Counter, MyForm 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import Greetings from './Greetings';
+import Counter from './Counter';
+import MyForm from './MyForm';
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
-  return <Greetings name="BKJang" onClick={onClick} />;
+  
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+
+  return (
+    <div>
+      <Greetings name="BKJang" onClick={onClick} />
+      <Counter />
+      <MyForm onSubmit={onSubmit} />
+    </div>
+  );
 }
 
 export default App;
