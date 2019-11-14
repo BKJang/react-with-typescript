7500c552baad2a7cacaeb051be2850ddebc5b72d
BKJang <jp302119@gmail.com>
2019-11-14 20:15:24 +0900

    🧩Add contextApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import './App.css';
-import Greetings from './Greetings';
-import Counter from './Counter';
-import MyForm from './MyForm';
 import ReducerSample from './ReducerSample';
+import { SampleProvider } from './SampleContext';
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-  
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
-
   return (
-    <div>
-      <Greetings name="BKJang" onClick={onClick} />
-      <Counter />
-      <MyForm onSubmit={onSubmit} />
+    <SampleProvider>
       <ReducerSample />
-    </div>
+    </SampleProvider>
   );
-}
+};
 
 export default App;
